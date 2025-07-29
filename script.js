af0894c692ed129531c2525d3a88bcf3269a6ec7
Johniz12 <114376958+Johniz12@users.noreply.github.com>
2025-07-29 14:59:52 +0800

    added active link

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('sticky');
     }
 });
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.navbar a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 60;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link =>{
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#$(current)`) {
+            link.classList.add('active');
+        }
+    });
+});
