bed6d0764e55dd6739766ab5a8de1f316865f2f4
Johniz12 <114376958+Johniz12@users.noreply.github.com>
2025-07-30 15:56:29 +0800

    added modal feature

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ window.addEventListener('scroll', () => {
     }
     });
 });
+
+const modal = document.getElementById('modal');
+const openModal = document.getElementById('openModal');
+const closeModal = document.getElementById('closeModal');
+
+openModal.addEventListener('click', () =>{
+    modal.classList.add('show')
+    console.log('modal open')
+})
+
+closeModal.addEventListener('click', () =>{
+    modal.classList.remove('show');
+});
+
+modal.addEventListener('click', (e) =>{
+    if (e.target === modal) {
+        modal.classList.remove('show');
+    }
+})
